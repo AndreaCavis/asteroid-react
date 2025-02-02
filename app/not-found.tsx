7779fdc8cd025111ea0e9f46bf85ba4e4c5a58e1
Andrea Canestrari <andrea_4991@hotmail.it>
2025-02-02 09:51:36 +0000

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,15 +4,14 @@ import Link from "next/link";
 export default function NotFoundPage() {
   return (
     <div className="container mx-auto p-4 flex flex-col items-center h-screen">
-      <Image src={"/asteroid-icon-pink.png"} alt={"Asteroid picture"} width={250} height={250} />
       <h1 className="text-8xl font-bold mb-6">404</h1>
       <h2 className="text-4xl text-white font-normal mb-4">
-        Sorry, an asteroid destroyed this page, it can't be found anymore.
+        Sorry, an asteroid destroyed this page. It can't be found anymore.
       </h2>
-      <br />
       <Link href={"/"} className="text-4xl underlined">
         Return to Home
       </Link>
+      <Image src={"/asteroid-icon-pink.png"} alt={"Asteroid picture"} width={250} height={250} />
     </div>
   );
 }
