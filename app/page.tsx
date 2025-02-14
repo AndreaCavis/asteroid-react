0b735419f85aca186b7c5dd1862ff3fda594464d
Andrea Canestrari <andrea_4991@hotmail.it>
2025-02-14 21:48:30 +0000

    inserted hover inside "parent class"

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import Product from "./components/products";
+import Searchbar from "./components/searchbar";
 
 export default async function Home() {
   const response = await fetch("http://localhost:3000/api/products");
   const products = await response.json();
   return (
     <>
-      <h1>Welcome to Asteroid</h1>
-      <br />
-      <h1>Products List</h1>
-      <br />
+      <h1 className="text-white text-4xl p-2 text-center">Welcome to Asteroid</h1>
+      <Searchbar />
       <Product products={products} />
     </>
   );
