9bbf625e1df1528c6a0f9ee9543eab80c408f7b8
Andrea Canestrari <andrea_4991@hotmail.it>
2025-02-14 22:32:28 +0000

    searchbar design

--- app/components/searchbar.tsx
+++ app/components/searchbar.tsx
@@ -3,7 +3,11 @@ export default function Searchbar() {
     <div className="flex justify-center">
       <form className="w-3/5 relative">
         <div className="relative ">
-          <input type="search" placeholder="Search here" className="w-full p-4 rounded-full bg-transparent" />
+          <input
+            type="search"
+            placeholder="Search here..."
+            className="w-full m-8 p-3 rounded-full bg-transparent text-white search-shadow "
+          />
         </div>
       </form>
     </div>
