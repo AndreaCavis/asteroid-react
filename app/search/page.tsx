9597351c8e8c01ff71ab89a651023313e22eed95
Andrea Canestrari <andrea_4991@hotmail.it>
2025-02-16 19:01:20 +0000

    Delete page.tsx

--- app/search/page.tsx
+++
@@ -1,9 +0,0 @@
-import ProductsList from "../components/productsList";
-import { Product } from "../product-data";
-
-export default function Results() {
-  return (
-    // <ProductsList />
-    <h1>Products</h1>
-  );
-}
