8183add5b849379f2dafd4c0cd56da2ea87acdff
Andrea Canestrari <andrea_4991@hotmail.it>
2025-02-16 19:01:17 +0000

    Create page.tsx

---
+++ app/search/[name]/page.tsx
@@ -0,0 +1,9 @@
+import ProductsList from "../components/productsList";
+import { Product } from "../product-data";
+
+export default function Results() {
+  return (
+    // <ProductsList />
+    <h1>Products</h1>
+  );
+}
