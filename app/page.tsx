670d8f6e58d33201b230dbb92daa3336e01ba5d6
Andrea Canestrari <andrea_4991@hotmail.it>
2025-03-13 05:01:12 +0000

    added sort button (layout only)

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { ChevronDown } from "lucide-react";
 import ProductsList from "../components/ui/productsList";
 import Searchbar from "../components/ui/searchbar";
 
@@ -5,10 +7,25 @@ export default async function Home() {
   const response = await fetch("http://localhost:3000/api/products");
   const products = await response.json();
   return (
-    <>
-      <h1 className="text-white text-4xl text-center">Welcome to Asteroid!</h1>
-      <Searchbar />
+    <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="flex items-baseline justify-between border-b-2 border-stone-600 pb-4">
+        <h1 className="text-4xl font-semibold tracking-tight text-white">
+          Find the <span className="text-[#EA3680] underlined">supplement</span> you're looking for
+        </h1>
+      </div>
+
+      <div className="flex items-center">
+        <div className="flex-1 flex justify-center">
+          <Searchbar />
+        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="group inline-flex justify-center text-md font-medium text-gray-200 hover:text-white">
+            Sort
+            <ChevronDown className="-mr-1 ml-1 w-5 font-extrabold flex-shrink-0 text-[#ff80ab] group-hover:text-[#EA3680]" />
+          </DropdownMenuTrigger>
+        </DropdownMenu>
+      </div>
       <ProductsList products={products} />
-    </>
+    </main>
   );
 }
