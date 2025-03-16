1c2e282c5e5c4fa5f55d9f98ef7716fc280f0978
Andrea Canestrari <andrea_4991@hotmail.it>
2025-03-16 04:56:30 +0000

    removed comments

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,15 +1,10 @@
 import { connectToDB } from "../mongoDB"
 
 export async function GET() {
-
     const { db } = await connectToDB();
     
-    // console.log("DB Name:", db.databaseName);  // debug purposes
-
     try {
     const products = await db.collection("products").find({}).toArray();
-
-    // console.log("THESE ARE THE PRODUCTS: ", products);  // debug purposes
 
     return new Response(JSON.stringify(products), {
         status: 200,
