9a4a1a2355a8ccdb29dc3b42a9f20acc26f4ce23
Andrea Canestrari <andrea_4991@hotmail.it>
2025-03-16 02:26:47 +0000

    export SORT_OPTIONS to use in homepage

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -1,9 +1,14 @@
+"use client";
+import { useState } from "react";
+
+export const SORT_OPTIONS = [
+  { name: "None", value: "none" },
+  { name: "Price: Low to High", value: "price-asc" },
+  { name: "Price: High to Low", value: "price-desc" },
+] as const; // as const creates read-only const
+
 export default function Sidebar() {
-  const SORT_OPTIONS = [
-    { name: "None", value: "none" },
-    { name: "Price: Low to High", value: "price-asc" },
-    { name: "Price: High to Low", value: "price-desc" },
-  ] as const;
+  const [filter, useFilter] = useState(); // FOR SORT_OPTION
 
   return (
     <div className="w-1/5 flex-none border-r-2 border-pink-800 min-h-full">
