448281320c0a920b947dfd934e784f16cff2d5b5
Andrea Canestrari <andrea_4991@hotmail.it>
2025-03-16 19:25:51 +0000

    improved skeleton layout

--- components/Products/ProductDetailsSkeleton.tsx
+++ components/Products/ProductDetailsSkeleton.tsx
@@ -1,20 +1,27 @@
+import { FaArrowLeftLong } from "react-icons/fa6";
+
 const ProductDetailsSkeleton = () => {
   return (
-    <div className="container mx-auto p-8 flex flex-col md:flex-row animate-pulse">
-      {/* 🖼 Image Skeleton */}
-      <div className="md:w-1/2 mb-4 md:mb-0 flex justify-center md:mr-8">
-        <div className="w-4/6 h-[400px] bg-stone-800 rounded-3xl ring-4 pink-shadow" />
+    <>
+      <div className="mt-4 mr-auto ml-12 text-stone-800 text-4xl animate-pulse">
+        <FaArrowLeftLong />
       </div>
+      <div className="container mx-auto p-8 flex flex-col md:flex-row animate-pulse">
+        {/* 🖼 Image Skeleton */}
+        <div className="md:w-1/2 mb-4 md:mb-0 flex justify-center md:mr-8">
+          <div className="w-4/6 h-[400px] bg-stone-800 rounded-3xl ring-4 pink-shadow" />
+        </div>
 
-      {/* 📜 Text Skeleton */}
-      <div className="md:w-1/2">
-        <div className="h-10 bg-stone-800 rounded-md mb-6 w-3/4" />
-        <div className="h-8 bg-stone-800 rounded-md mb-8 w-2/3" />
-        <div className="h-6 bg-stone-800 rounded-md mb-8 w-1/2" />
-        <div className="h-8 bg-stone-800 rounded-md mb-4 w-1/4" />
-        <div className="h-6 bg-stone-800 rounded-md w-full" />
+        {/* 📜 Text Skeleton */}
+        <div className="md:w-1/2">
+          <div className="h-10 bg-stone-800 rounded-md mb-6 w-3/4" />
+          <div className="h-8 bg-stone-800 rounded-md mb-8 w-2/3" />
+          <div className="h-6 bg-stone-800 rounded-md mb-8 w-1/2" />
+          <div className="h-8 bg-stone-800 rounded-md mb-4 w-1/4" />
+          <div className="h-6 bg-stone-800 rounded-md w-full" />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
