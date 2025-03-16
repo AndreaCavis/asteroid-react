b408a2a2d87858600970b07d1f03f3af514e088d
Andrea Canestrari <andrea_4991@hotmail.it>
2025-03-16 00:25:51 +0000

    fixed href navbar

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="pink-shadow fixed min-h-0 max-h-20 bg-black z-50">
+    <nav className="pink-shadow fixed min-h-0 max-h-24 bg-black z-50">
       <Link href={"/"}>
         <img
           src={"/asteroid-pink.png"}
           alt={"Asteroid logo"}
           width={334}
           height={100}
-          className="w-1/6 h-auto p-2 duration-500 hover:scale-105"
+          className="w-1/6 h-auto m-2 inline-block duration-500 hover:scale-105"
         />
       </Link>
     </nav>
