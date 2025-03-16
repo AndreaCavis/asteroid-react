b8206e2a2987a83d2244de6ef9012ef4ec999b4d
Andrea Canestrari <andrea_4991@hotmail.it>
2025-03-16 19:26:12 +0000

    added second filter checkbox

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "./accordion";
 import { ProductState } from "@/lib/validators/product-validator";
+import { Item } from "@radix-ui/react-accordion";
 
 export const SORT_OPTIONS = [
   { name: "None", value: "none" },
@@ -17,8 +18,18 @@ const TYPE_FILTERS = {
     { value: "beta alanine", label: "Beta-Alanine" },
     { value: "creatine", label: "Creatine" },
     { value: "whey protein", label: "Whey Protein" },
-  ],
+  ] as const,
 };
+
+const BRAND_FILTERS = {
+  id: "brand",
+  name: "Brand",
+  options: [
+    { value: "MyProtein", label: "MyProtein" },
+    { value: "Optimum Nutrition", label: "Optimum Nutrition" },
+    { value: "Yamamoto Nutrition", label: "Yamamoto Nutrition" },
+  ],
+} as const;
 
 // to display which category of products is being displayed, not implemented
 const SUPPLEMENT_TYPES = [
@@ -31,7 +42,9 @@ const SUPPLEMENT_TYPES = [
 const DEFAULT_CUSTOM_PRICE = [0, 100] as [number, number];
 
 const Sidebar = () => {
-  const [filter, useFilter] = useState<ProductState>({
+  const [openSections, setOpenSections] = useState<string[]>(["type", "brand"]); // ACCORDION TOGGLE
+
+  const [filter, setFilter] = useState<ProductState>({
     // default filters allow all the available options
     type: ["bcaa", "beta alanine", "creatine", "whey protein"],
     price: { isCustom: false, range: DEFAULT_CUSTOM_PRICE },
@@ -39,14 +52,46 @@ const Sidebar = () => {
     sort: "none",
   });
 
+  console.log(filter);
+
+  // array filter (no price) handler
+  const applyArrayFilters = ({
+    category,
+    value,
+  }: {
+    category: keyof Omit<typeof filter, "price" | "sort">;
+    value: string; // values inside arrays (eg: bcaa, beta alanine,... || MyProtein, Optimum Nutrition,...)
+  }) => {
+    const isFilterApplied = filter[category].includes(value as never);
+
+    // checks for value in array and creates a copy with updates value
+    // Javascript Garbage Collector removes unreferenced arrays from memory, therefore each iteration creates a new array and deletes the old one
+    if (isFilterApplied) {
+      setFilter((prev) => ({
+        ...prev,
+        // if value was previously in array, we acces it with prev[category] and remove it by keeping all the values different from the selected one
+        [category]: prev[category].filter((v) => v !== value),
+      }));
+    } else {
+      setFilter((prev) => ({
+        ...prev,
+        // if value wasn't in array, we append it at the end of it
+        [category]: [...prev[category], value],
+      }));
+    }
+  };
+
   return (
-    <div className="w-1/5 pl-2 flex-none  min-h-full">
-      <h1>This is the sidebar</h1>
-      <Accordion type="multiple">
-        {/* Type filter (bcaa, creatine, etc) */}
+    <div className="w-1/5 pl-2 pr-2 border-r-2 border-pink-800/50 flex-none min-h-full">
+      <Accordion
+        type="multiple"
+        value={openSections}
+        onValueChange={(newValues) => setOpenSections(newValues)} // Allows toggle
+      >
+        {/* Supplement (type) filters (bcaa, creatine, etc) */}
         <AccordionItem value="type">
-          <AccordionTrigger className="py-3">
-            <span className="font-medium text-white group-hover:text-[var(--primary)] text-lg">Type</span>
+          <AccordionTrigger className="py-3" defaultValue="item">
+            <span className="font-medium text-white group-hover:text-[var(--primary)] text-lg">Supplement</span>
           </AccordionTrigger>
           <AccordionContent>
             <ul className="space-y-4">
@@ -55,10 +100,50 @@ const Sidebar = () => {
                   <input
                     type="checkbox"
                     id={`type-${optionIdx}`}
-                    className="peer h-4 w-4 rounded border-gray-300 text-indigo-200 focus:ring-indigo-900"
+                    onChange={() => {
+                      applyArrayFilters({
+                        category: "type",
+                        value: option.value,
+                      });
+                    }}
+                    checked={filter.type.includes(option.value)}
+                    className="peer h-4 w-4 rounded-sm hover:[accent-color]"
                   />
                   <label
                     htmlFor={`type-${optionIdx}`}
+                    className="ml-3 text-base text-stone-400 peer-hover:text-white peer-checked:text-white"
+                  >
+                    {option.label}
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </AccordionContent>
+        </AccordionItem>
+
+        {/* Brand filters */}
+        <AccordionItem value="brand">
+          <AccordionTrigger className="py-3" defaultValue="item">
+            <span className="font-medium text-white group-hover:text-[var(--primary)] text-lg">Brand</span>
+          </AccordionTrigger>
+          <AccordionContent>
+            <ul className="space-y-4">
+              {BRAND_FILTERS.options.map((option, optionIdx) => (
+                <li key={option.value} className="flex items-center">
+                  <input
+                    type="checkbox"
+                    id={`brand-${optionIdx}`}
+                    onChange={() => {
+                      applyArrayFilters({
+                        category: "brand",
+                        value: option.value,
+                      });
+                    }}
+                    checked={filter.brand.includes(option.value)}
+                    className="peer h-4 w-4 rounded border-gray-300 text-indigo-200 focus:ring-indigo-900"
+                  />
+                  <label
+                    htmlFor={`brand-${optionIdx}`}
                     className="ml-3 text-base text-stone-400 peer-hover:text-white peer-checked:text-white"
                   >
                     {option.label}
