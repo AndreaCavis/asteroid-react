c61ac7ed5053de71ceb02f6f50e77c87b8752027
Andrea Canestrari <andrea_4991@hotmail.it>
2025-03-16 02:27:11 +0000

    sort function layout setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,32 @@
-import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+"use client";
+
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { ChevronDown } from "lucide-react";
 import ProductsList from "../components/ui/productsList";
 import Searchbar from "../components/ui/searchbar";
+import { SORT_OPTIONS } from "../components/ui/sidebar";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
 
-export default async function Home() {
-  const response = await fetch("http://localhost:3000/api/products");
-  const products = await response.json();
+export default function Home() {
+  const [filter, setFilter] = useState({
+    sort: "none",
+  });
+  console.log(filter);
+
+  const [products, setProducts] = useState([]);
+
+  // async function to handle database call in use client
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("http://localhost:3000/api/products");
+      const products = await response.json();
+      setProducts(products);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="flex items-baseline justify-between border-b-2 border-stone-600 pb-4">
@@ -13,7 +34,6 @@ export default async function Home() {
           Find the <span className="text-[#EA3680] underlined">supplement</span> you're looking for
         </h1>
       </div>
-
       <div className="flex items-center">
         <div className="flex-1 flex justify-center">
           <Searchbar />
@@ -23,6 +43,25 @@ export default async function Home() {
             Sort
             <ChevronDown className="-mr-1 ml-1 w-5 font-extrabold flex-shrink-0 text-[#ff80ab] group-hover:text-[#EA3680]" />
           </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {SORT_OPTIONS.map((option) => (
+              <button
+                key={option.name}
+                className={cn("text-left w-full block px-4 py-2", {
+                  "text-white bg-stone-800": option.value === filter.sort,
+                  "text-gray-400": option.value !== filter.sort,
+                })}
+                onClick={() => {
+                  setFilter((prev) => ({
+                    ...prev,
+                    sort: option.value,
+                  }));
+                }}
+              >
+                {option.name}
+              </button>
+            ))}
+          </DropdownMenuContent>
         </DropdownMenu>
       </div>
       <ProductsList products={products} />
