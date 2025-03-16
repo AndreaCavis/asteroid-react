5097319dac31d9b9673c4084f26791cfaf112189
Andrea Canestrari <andrea_4991@hotmail.it>
2025-03-16 17:40:56 +0000

    Fixed navbar shadow

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="pink-shadow fixed min-h-0 max-h-24 bg-black z-50">
+    <nav className="navbar-shadow fixed top-0 left-0 w-full h-24 bg-black z-50">
       <Link href={"/"}>
         <img
           src={"/asteroid-pink.png"}
           alt={"Asteroid logo"}
           width={334}
           height={100}
-          className="w-1/6 h-auto m-2 inline-block duration-100 hover:opacity-75"
+          className="w-1/6 h-auto m-2 inline-block duration-200 hover:opacity-75"
         />
       </Link>
     </nav>
