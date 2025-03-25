02c234c30ade13602854f94d48fbdae5bf626c8f
Andrea Canestrari <andrea_4991@hotmail.it>
2025-03-25 18:04:56 +0000

    fixed card skeleton

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -9,6 +9,7 @@ import { cn } from "@/lib/utils";
 import { RESET_FILTERS, SORT_OPTIONS, useFilters } from "@/components/FiltersContext";
 import ProductCard from "@/components/Products/ProductCard";
 import EmptySearchState from "@/components/Products/EmptySearchState";
+import ProductCardSkeleton from "@/components/Products/ProductCardSkeleton";
 
 export default function Results() {
   const searchParams = useSearchParams();
@@ -72,7 +73,7 @@ export default function Results() {
         <div className="block">
           <div className="m-4 grid gap-x-4 gap-y-8 lg:grid-cols-[repeat(auto-fit,minmax(14rem,1fr))] md:grid-cols-3 sm:grid-cols-3 grid-cols-3">
             {!products
-              ? new Array(12).map((_, i) => <ProductSkeleton key={i} />)
+              ? new Array(12).map((_, i) => <ProductCardSkeleton key={i} />)
               : products.map((product) => <ProductCard key={product.id} product={product} />)}
           </div>
         </div>
