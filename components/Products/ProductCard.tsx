559a2b88a9e98e29934434862b67d970b9d1f72a
Andrea Canestrari <andrea_4991@hotmail.it>
2025-04-06 07:09:35 +0100

    Product Card update

--- components/Products/ProductCard.tsx
+++ components/Products/ProductCard.tsx
@@ -4,26 +4,30 @@ import Link from "next/link";
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
-    <Link
-      key={product.id}
-      href={"/products/" + product.id}
-      className="bg-none rounded-md w-52 opacity-75 hover:opacity-100 hover:scale-95 transition-all duration-300 ease"
-    >
+    <Link key={product.id} href={"/products/" + product.id} className="group bg-none rounded-md w-52 ">
       <div className="flex justify-center lg:w-52 lg:h-52 md:w-44 md:h-44 sm:w-32 sm:h-32 w-24 h-24 overflow-hidden">
         <Image
           src={"/" + product.imageUrl}
           alt={product.name + " image"}
           width={208}
           height={208}
-          className="object-fill rounded-md lg:w-52 lg:h-52 md:w-44 md:h-44 sm:w-32 sm:h-32 w-24 h-24"
+          className="object-fill rounded-md lg:w-52 lg:h-52 md:w-44 md:h-44 sm:w-32 sm:h-32 w-24 h-24
+          group-hover:opacity-100 scale-90 opacity-75 group-hover:scale-100 transition-all duration-300 ease"
         />
       </div>
-      <div className="p-2 lg:w-52 md:w-44 sm:w-32 w-24 lg:text-lg md:text-base sm:text-sm text-xs">
-        <h2 className="lg:text-xl md:text-lg sm:text-base text-sm font-semibold">{product.name}</h2>
-        <h3 className="text-accent-foreground font-normal mb-1">{product.brand}</h3>
-        <p className="text-primary font-semibold">
-          <span className="text-accent-foreground">£</span> {product.price}
-        </p>
+
+      <div className=" group-hover:opacity-100">
+        <div className="p-2 lg:w-52 md:w-44 sm:w-32 w-24 lg:text-lg md:text-base sm:text-sm text-xs">
+          <h2 className="transition-opacity duration-200 opacity-50 group-hover:opacity-100 lg:text-xl md:text-lg sm:text-base text-sm font-semibold">
+            {product.name}
+          </h2>
+          <h3 className="transition-all duration-100 opacity-50 group-hover:opacity-100 text-accent-foreground font-normal mb-1">
+            {product.brand}
+          </h3>
+          <p className="text-accent-foreground font-semibold opacity-50 transition-all duration-0 group-hover:opacity-100">
+            £<span className="text-primary">{product.price}</span>
+          </p>
+        </div>
       </div>
     </Link>
   );
