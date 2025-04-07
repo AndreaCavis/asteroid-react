af4420ab50761ae11dea7a09b7c26144082757c1
Andrea Canestrari <andrea_4991@hotmail.it>
2025-04-07 01:10:01 +0100

    updated layout

--- components/Products/ProductCard.tsx
+++ components/Products/ProductCard.tsx
@@ -12,20 +12,23 @@ const ProductCard = ({ product }: { product: Product }) => {
           width={208}
           height={208}
           className="object-fill rounded-md lg:w-52 lg:h-52 md:w-44 md:h-44 sm:w-32 sm:h-32 w-24 h-24
-          group-hover:opacity-100 scale-90 opacity-75 group-hover:scale-100 transition-all duration-300 ease"
+          group-hover:opacity-75 scale-90 opacity-50 group-hover:scale-100 transition-all duration-300 ease"
         />
       </div>
 
-      <div className=" group-hover:opacity-100">
+      <div className="group-hover:opacity-100">
         <div className="p-2 lg:w-52 md:w-44 sm:w-32 w-24 lg:text-lg md:text-base sm:text-sm text-xs">
-          <h2 className="transition-opacity duration-200 opacity-50 group-hover:opacity-100 lg:text-xl md:text-lg sm:text-base text-sm font-semibold">
+          <h2
+            className="animated-text transition-opacity duration-200 opacity-75 group-hover:opacity-100 
+          lg:text-xl md:text-lg sm:text-base text-sm font-semibold"
+          >
             {product.name}
           </h2>
-          <h3 className="transition-all duration-100 opacity-50 group-hover:opacity-100 text-accent-foreground font-normal mb-1">
+          <h3 className="transition-all duration-200 opacity-75 group-hover:opacity-100 text-accent-foreground font-normal mb-1">
             {product.brand}
           </h3>
-          <p className="text-accent-foreground font-semibold opacity-50 transition-all duration-0 group-hover:opacity-100">
-            £<span className="text-primary">{product.price}</span>
+          <p className="text-accent-foreground font-semibold opacity-75 transition-all duration-0 group-hover:opacity-100">
+            £ <span className="text-primary">{product.price}</span>
           </p>
         </div>
       </div>
--- lib/validators/product-validator.ts
+++ lib/validators/product-validator.ts
@@ -1,4 +1,4 @@
-import {z} from "zod";
+import { z } from "zod";
 
 export const AVAILABLE_TYPES = ["bcaa", "beta alanine", "creatine", "whey protein"] as const;
 
@@ -7,27 +7,26 @@ export const AVAILABLE_BRANDS = ["Optimum Nutrition", "MyProtein", "Yamamoto Nut
 export const AVAILABLE_SORT = ["none", "price-asc", "price-desc"] as const;
 
 export const ProductValidator = z.object({
-    id: z.string(),
-    imageUrl: z.string().regex(/^\/?public\/.+/, "Invalid image path"), // Ensures it starts with 'public/'
-    brand: z.string(),
-    name: z.string(),
-    type: z.string(),
-    suggested_use: z.string(),
-    price: z.number().positive(),
-  });
+  id: z.string(),
+  imageUrl: z.string().regex(/^\/?public\/.+/, "Invalid image path"), // Ensures it starts with 'public/'
+  brand: z.string(),
+  name: z.string(),
+  type: z.string(),
+  suggested_use: z.string(),
+  price: z.number().positive(),
+});
 
-  export type Product = z.infer<typeof ProductValidator>;
-
+export type Product = z.infer<typeof ProductValidator>;
 
 // schema to enforce shape on back-end and full type safety on front-end
 export const ProductFilterValidator = z.object({
-    type: z.array(z.enum(AVAILABLE_TYPES)),
-    brand: z.array(z.enum(AVAILABLE_BRANDS)),
-    sort: z.enum(AVAILABLE_SORT),
-    price: z.tuple([z.number(), z.number()])
+  type: z.array(z.enum(AVAILABLE_TYPES)),
+  brand: z.array(z.enum(AVAILABLE_BRANDS)),
+  sort: z.enum(AVAILABLE_SORT),
+  price: z.tuple([z.number(), z.number()]),
 });
 
 // Convert Zod Schema to TypeScript Type. price handled separately due to custom value
 export type ProductState = Omit<z.infer<typeof ProductFilterValidator>, "price"> & {
-    price: { isCustom: boolean, range: [number, number] }
-}
+  price: { isCustom: boolean; range: [number, number] };
+};
