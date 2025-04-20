b27616b07414b64d5c72442941c3c9e9803bd219
Andrea Canestrari <andrea_4991@hotmail.it>
2025-04-20 06:32:40 +0100

    top right shadow image effect

--- components/Products/ProductCard.tsx
+++ components/Products/ProductCard.tsx
@@ -5,14 +5,21 @@ import Link from "next/link";
 const ProductCard = ({ product }: { product: Product }) => {
   return (
     <Link key={product.id} href={"/products/" + product.id} className="group bg-none rounded-md w-52">
-      <div className="flex justify-center lg:w-52 lg:h-52 md:w-44 md:h-44 sm:w-32 sm:h-32 w-24 h-24 overflow-hidden">
+      <div
+        className="flex justify-center lg:w-52 lg:h-52 md:w-44 md:h-44 sm:w-32 sm:h-32 w-24 h-24 overflow-hidden
+                      rounded-lg transition duration-300   bg-stone-800/10
+                      group-hover:bg-gradient-to-tr group-hover:from-stone-300/90 group-hover:via-stone-300/50 group-hover:to-stone-500/20
+                      after:group-hover:bg-gradient-to-tl after:group-hover:from-stone-300/90 after:group-hover:via-stone-300/50 after:group-hover:to-stone-500/20
+                      group-hover:rounded-md"
+      >
         <Image
           src={"/" + product.imageUrl}
           alt={product.name + " image"}
           width={208}
           height={208}
-          className="object-fill rounded-md lg:w-52 lg:h-52 md:w-44 md:h-44 sm:w-32 sm:h-32 w-24 h-24
-          group-hover:opacity-100 scale-90 opacity-75 group-hover:scale-100 transition-all duration-300 ease"
+          className="object-fill lg:w-52 lg:h-52 md:w-44 md:h-44 sm:w-32 sm:h-32 w-24 h-24
+                     opacity-75 scale-95 rounded-md transition-all duration-300 ease
+                     group-hover:opacity-60 group-hover:scale-105"
         />
       </div>
 
