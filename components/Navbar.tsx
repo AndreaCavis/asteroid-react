c815630940d809de74bfae73c3807d32be98b0aa
Andrea Canestrari <andrea_4991@hotmail.it>
2025-04-21 03:19:26 +0100

    LAYOUT NAVBAR BLUE

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,8 +4,9 @@ const Navbar = () => {
   return (
     // semi-transparent nav-bar: bg-gradient-to-b from-black via-black/90 to-black/70
     <nav
-      className="navbar max-h-24 backdrop-blur-sm bg-gradient-to-b from-black/60 via-black/40 to-black/60
-     hover:bg-black/90 transition-all duration-200"
+      className="navbar max-h-24 backdrop-blur-sm transition-all duration-200
+                  bg-gradient-to-r from-black/30 via-black/80 to-black/90
+                  hover:bg-black/90"
     >
       <Link href={"/"}>
         <img
@@ -13,7 +14,7 @@ const Navbar = () => {
           alt={"Asteroid logo"}
           width={334}
           height={100}
-          className="w-1/6 h-auto m-2 inline-block duration-200 hover:animate-pulse"
+          className="z-50 w-1/6 h-auto m-2 inline-block transition-all duration-50 hover:animate-pulse hover:duration-1000"
         />
       </Link>
     </nav>
