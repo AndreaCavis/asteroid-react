83923cc4909c2d9ec6cd8da21e32a6dda58b66b8
Andrea Canestrari <andrea_4991@hotmail.it>
2025-04-29 17:30:49 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // REMOVE eslint and typescript for build error-free deployment
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
+  //---------------------
 };
 
 export default nextConfig;
